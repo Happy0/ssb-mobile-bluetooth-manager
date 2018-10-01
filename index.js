0b9701c2943f9c9b1a78609a0bea2bde9023907a
Gordon Martin <gordon.hugh.martin@gmail.com>
2018-10-01 14:22:26 +0100

    Handle incoming connections.

--- index.js
+++ index.js
@@ -8,15 +8,17 @@ const Abortable = require('pull-abortable');
 const net = require('net');
 const toPull = require('stream-to-pull-stream');
 
+const fs = require('fs')
+
 function makeManager () {
 
   function connect(address, cb) {
 
     var started = false;
 
-    var socket = net.connect({
-      path: "\0manyverse_bt_outgoing.sock"
-    }).on('connect', function () {
+    var socket = net.connect(
+      "/data/data/se.manyver/files/manyverse_bt_outgoing.sock"
+    ).on('connect', function () {
       if(started) return
 
       // Tell the other side of the bridge the bluetooth device to connect to.
@@ -40,9 +42,44 @@ function makeManager () {
 
   }
 
+  // hax on double transform
+  var started = false
+
   function listenForIncomingConnections(onConnection) {
 
-    // todo
+    if(started) return
+    
+    var socket = "/data/data/se.manyver/files/manyverse_bt_incoming.sock";
+
+    var server = net.createServer(function (stream) {
+      onConnection(null, toPull.duplex(stream))
+    }).listen(socket);
+
+    server.on('error', function (e) {
+      if (e.code == 'EADDRINUSE') {
+        var clientSocket = new net.Socket()
+        clientSocket.on('error', function(e) {
+          if (e.code == 'ECONNREFUSED') {
+            fs.unlinkSync(socket)
+            server.listen(socket)
+          }
+        })
+
+        clientSocket.connect({ path: socket }, function() {
+          console.log("bt-bridge: someone else is listening on socket!")
+        })
+      } else {
+        console.log("bt_bridge: " + e);
+      }
+    })
+
+    server.on('close', function (e) {
+      console.log("bt_bridge socket closed: " + e);
+    });
+
+    started = true;
+
+    fs.chmodSync(socket, 0600)
 
     var bridgeMsg = {
       type: "listenIncoming",
@@ -51,6 +88,10 @@ function makeManager () {
 
     rn_bridge.channel.send(JSON.stringify(bridgeMsg));
     
+    return function () {
+      console.log("Server close?");
+      server.close()
+    }
   }
 
   return {
