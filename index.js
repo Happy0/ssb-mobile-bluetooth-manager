29aea58ffd31d7ee193eff535ddc11ce81fd0007
Gordon Martin <gordon.hugh.martin@gmail.com>
2019-03-11 16:52:09 +0000

    dfdsf

--- index.js
+++ index.js
@@ -324,13 +324,14 @@ function makeManager (opts) {
 
       outgoingConnectionsEstablished.push(item);
 
-    }).listen(address);
+    });
 
     server.on('listening', () => {
       debug("Server listening for outgoing connections. Starting control unix socket.");
       makeControlSocket();
     });
 
+    return server.listen(address);
   }
 
   // For some reason, .server gets called twice...
